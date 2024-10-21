0efda24bc50e7ad37576be6e9c7617a3ce4eb222
Vinci <1341060890@qq.com>
2024-10-21 21:34:02 +0800

    chore: 初始化项目

---
+++ yunying-server/src/main/java/com/yunying/server/YunyingServerApplication.java
@@ -0,0 +1,13 @@
+package com.yunying.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunyingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunyingServerApplication.class, args);
+    }
+
+}
