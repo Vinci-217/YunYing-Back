2c553b35c994e56a4003f58828c98caa3188eea7
Vinci-217 <1341060890@qq.com>
2024-10-23 09:46:02 +0800

    feat: 引入网关

---
+++ yunying-gateway/src/main/java/com/yunying/gateway/YunyingGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yunying.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunyingGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunyingGatewayApplication.class, args);
+    }
+
+}
