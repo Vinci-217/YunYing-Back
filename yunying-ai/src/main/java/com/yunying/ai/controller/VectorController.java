62f80ce9e876159db15b5190beb884cb2dd94412
Vinci-217 <1341060890@qq.com>
2024-10-24 16:16:13 +0800

    feat: 实现了RAG的功能

--- yunying-ai/src/main/java/com/yunying/ai/controller/VectorController.java
+++ yunying-ai/src/main/java/com/yunying/ai/controller/VectorController.java
@@ -13,13 +13,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/ai")
 public class VectorController {
 
     @Autowired
-    private VectorStore vectorStore;
+    private MilvusVectorStore vectorStore;
 
     @Autowired
     private EmbeddingModel embeddingModel;
@@ -27,27 +28,23 @@ public class VectorController {
     @GetMapping("/embedding")
     public Map embed(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         EmbeddingResponse embeddingResponse = embeddingModel.embedForResponse(List.of(message));
-        return Map.of("embedding", embeddingResponse);
+        return Map.of("embedding", embeddingResponse.getResults());
     }
 
     @PostMapping("/vector")
     public List<Document> search(@RequestParam("query") String query) {
-        List<Document> documents = new ArrayList<>();
-        // 示例文本数据
-        String text1 = "Spring AI rocks!!";
-        String text2 = "The World is Big and Salvation Lurks Around the Corner";
-        String text3 = "You walk forward facing the past and you turn back toward the future.";
 
-        // 为每个文本生成向量并创建文档
-        documents.add(new Document(text1, Map.of("meta1", embeddingModel.embedForResponse(List.of(text1)))));
-        documents.add(new Document(text2, Map.of("meta2", embeddingModel.embedForResponse(List.of(text2)))));
-        documents.add(new Document(text3, Map.of("meta3", embeddingModel.embedForResponse(List.of(text3)))));
 
-        // 添加文档到 Milvus
-        vectorStore.add(documents);
+        List <Document> documents = List.of(
+                new Document("Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!", Map.of("meta1", "meta1")),
+                new Document("The World is Big and Salvation Lurks Around the Corner"),
+                new Document("You walk forward facing the past and you turn back toward the future.", Map.of("meta2", "meta2")));
 
-        // 执行搜索
-        SearchRequest searchRequest = SearchRequest.query(query).withTopK(5);
-        return vectorStore.similaritySearch(searchRequest);
+        vectorStore.doAdd(documents);
+
+        List<Document> results = vectorStore.similaritySearch(SearchRequest.query("Spring").withTopK(2));
+
+        System.out.println(results);
+        return results;
     }
 }
