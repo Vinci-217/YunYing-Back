a396f28137fcc285c1ee7163eb96fd42f9dadc8d
Vinci-217 <1341060890@qq.com>
2024-10-24 16:19:40 +0800

    feat: 实现了RAG的功能

--- yunying-ai/src/main/java/com/yunying/ai/controller/VectorController.java
+++ yunying-ai/src/main/java/com/yunying/ai/controller/VectorController.java
@@ -38,7 +38,7 @@ public class VectorController {
         List <Document> documents = List.of(
                 new Document("Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!", Map.of("meta1", "meta1")),
                 new Document("The World is Big and Salvation Lurks Around the Corner"),
-                new Document("You walk forward facing the past and you turn back toward the future.", Map.of("meta2", "meta2")));
+                new Document("原神是一款五星级游戏.", Map.of("meta2", "meta2")));
 
         vectorStore.doAdd(documents);
 
