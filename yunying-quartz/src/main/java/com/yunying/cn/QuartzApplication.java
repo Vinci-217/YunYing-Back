2f16aedb4e1c4d2d222857bd3341f0fe787d90c3
Vinci-217 <1341060890@qq.com>
2024-10-28 16:34:19 +0800

    refactor: 添加日志

--- yunying-quartz/src/main/java/com/yunying/cn/QuartzApplication.java
+++ yunying-quartz/src/main/java/com/yunying/cn/QuartzApplication.java
@@ -8,6 +8,7 @@ public class QuartzApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuartzApplication.class, args);
+        System.out.println("QuartzApplication started successfully!");
     }
 
 }
